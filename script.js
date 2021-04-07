3683f2c1ca2b3ff93ccc5faafd4d492bda0116ec
Aleksandr <dolopolov9@gmail.com>
2021-04-07 18:18:30 +0700

    feat: init

---
+++ script.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Time manager
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @downloadURL https://raw.githubusercontent.com/AleksDolgop/GetSummaryWindGismeteo/main/script.js
+// @updateURL https://raw.githubusercontent.com/AleksDolgop/GetSummaryWindGismeteo/main/script.js
+// @description  Собирает сумму ветров от направлений
+// @author       AleksDolgop
+// @match         https://www.gismeteo.ru/diary/*/*/*/
+// @grant        none
+// @require https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
